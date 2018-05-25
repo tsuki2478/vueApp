c59be9e09c5dc763690d52b62620c52268595eab
Cy <tsuki2478@outlook.com>
2018-05-25 16:46:34 +0800

    git好像用坏了

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- exclude 排除掉Detail组件。。 也就是不缓存Detail组件  -->
+    <!-- exclude 排除掉Detail组件。也就是不缓存Detail组件  -->
     <keep-alive exclude="detail">
       <router-view/>
     </keep-alive>
